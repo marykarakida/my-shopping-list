639c5a676dc9181cfcaa53f8b5770f30225d49df
Mary Karakida <makarakida@gmail.com>
2022-09-15 19:23:36 -0300

    tests done

--- tests/items.test.ts
+++ tests/items.test.ts
@@ -1,13 +1,57 @@
-describe('Testa POST /items ', () => {
-  it.todo('Deve retornar 201, se cadastrado um item no formato correto');
-  it.todo('Deve retornar 409, ao tentar cadastrar um item que exista');
-});
+import supertest from "supertest";
+import app from "../src/app";
+import {prisma} from "../src/database";
 
-describe('Testa GET /items ', () => {
-  it.todo('Deve retornar status 200 e o body no formato de Array');
-});
+const agent = supertest(app);
 
-describe('Testa GET /items/:id ', () => {
-  it.todo('Deve retornar status 200 e um objeto igual a o item cadastrado');
-  it.todo('Deve retornar status 404 caso não exista um item com esse id');
-});
+describe("/items", () => {
+  beforeAll(async () => {
+      await prisma.$executeRaw`TRUNCATE TABLE "items"`;
+  });
+
+  afterAll(async () => {
+      await prisma.$disconnect();
+  });
+
+  let itemId = undefined;
+
+  describe('Testa POST /items ', () => {
+    it('Deve retornar 201, se cadastrado um item no formato correto', async () => {
+      const newItem = {title: "Teste", url: "https://javascript.plainenglish.io/typed-express-request-and-response-with-typescript-7277aea028c", description: "Teste", amount: 1}
+      const result = await agent.post("/items").send(newItem)
+
+      itemId = result.body.id
+
+      expect(result.status).toBe(201)
+    });
+    it('Deve retornar 409, ao tentar cadastrar um item que exista', async () => {
+      const newItem = {title: "Teste", url: "https://javascript.plainenglish.io/typed-express-request-and-response-with-typescript-7277aea028c", description: "Teste", amount: 1}
+      const result = await agent.post("/items").send(newItem)
+
+      expect(result.status).toBe(409)
+    });
+  });
+  
+  describe('Testa GET /items ', () => {
+    it('Deve retornar status 200 e o body no formato de Array', async () => {
+      const result = await agent.get("/items")
+
+      expect(result.status).toBe(200)
+      expect(Array.isArray(result.body)).toBe(true)
+    });
+  });
+  
+  describe('Testa GET /items/:id ', () => {
+    it('Deve retornar status 200 e um objeto igual a o item cadastrado', async ()=> {
+      const result = await agent.get(`/items/${itemId}`)
+
+      expect(result.status).toBe(200)
+    });
+    it('Deve retornar status 404 caso não exista um item com esse id', async () =>{
+      const result = await agent.get("/items/10000000000")
+
+      expect(result.status).toBe(404)
+    });
+  });
+})
+
